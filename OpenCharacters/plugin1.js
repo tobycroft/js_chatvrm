b01bbfcea037b1c79133628051968364ad47c626
tuuz <silverhawk@foxmail.com>
2024-04-03 00:50:59 +0800

    TuuzPC

--- OpenCharacters/plugin1.js
+++ OpenCharacters/plugin1.js
@@ -2,8 +2,8 @@ let [
   Viewer,
   prompt2,
 ] = await Promise.all([
-  import("https://cdn.jsdelivr.net/gh/josephrocca/ChatVRM-js@v0.0.27/features/vrmViewer/viewer.js").then(m => m.Viewer),
-  import("https://cdn.jsdelivr.net/gh/josephrocca/prompt2@v0.0.8/mod.js").then(m => m.default),
+  import("https://vrm.ai.aerofsx.com:444/features/vrmViewer/viewer.js").then(m => m.Viewer),
+  import("//cdn.jsdelivr.net/gh/josephrocca/prompt2@v0.0.8/mod.js").then(m => m.default),
 ]);
 
 window.onerror = async function(errorMsg, url, lineNumber, columnNumber, errorObj) {
@@ -30,7 +30,7 @@ canvas.width = window.innerWidth;
 canvas.style.cssText = "width:100vw; height:100vh; display:block;";
 document.body.appendChild(canvas);
 
-let defaultVrmUrl = "https://ai.aerofsx.com:444/vrm/avatars/AvatarSample_B.vrm";
+let defaultVrmUrl = "//ai.aerofsx.com:444/vrm/avatars/AvatarSample_B.vrm";
 
 viewer.setup(canvas);
 await viewer.loadVrm(oc.character.customData.vrmUrl || defaultVrmUrl);
@@ -63,10 +63,10 @@ canvas.addEventListener("drop", function (event) {
 async function pluginSettings() {
   let originalVrmUrl = oc.character.customData.vrmUrl;
   let result = await prompt2({
-    vrmUrl: {type:"textLine", label: "Upload a VRM file to <a href='https://catbox.moe/'>catbox.moe</a> and put the URL here (url should end in <b>.vrm</b>). You can <b>make</b> a VRM character <a href='https://vroid.com/en/studio'>here</a>, or you can download one <a href='https://hub.vroid.com/en/models?is_other_users_available=1&is_downloadable=1'>here</a>.", defaultValue:oc.character.customData.vrmUrl || defaultVrmUrl},
+    vrmUrl: {type:"textLine", label: "Upload a VRM file to <a href='//catbox.moe/'>catbox.moe</a> and put the URL here (url should end in <b>.vrm</b>). You can <b>make</b> a VRM character <a href='//vroid.com/en/studio'>here</a>, or you can download one <a href='//hub.vroid.com/en/models?is_other_users_available=1&is_downloadable=1'>here</a>.", defaultValue:oc.character.customData.vrmUrl || defaultVrmUrl},
     voiceAudioEnabled: {type:"select", label: "Voice audio enabled? (requires ElevenLabs API key)", options:[{value:"no"}, {value:"yes"}], defaultValue:oc.character.customData.voiceAudioEnabled || "no"},
     elevenLabsVoiceId: {show:d=>d.voiceAudioEnabled=="yes", type:"textLine", label: "Enter an ElevenLabs voice ID:", defaultValue:oc.character.customData.elevenLabsVoiceId || "21m00Tcm4TlvDq8ikWAM"},
-    elevenLabsApiKey: {show:d=>d.voiceAudioEnabled=="yes", type:"textLine", label: "Enter your ElevenLabs API key (see <a href='https://beta.elevenlabs.io/speech-synthesis'>user settings</a>):", defaultValue:oc.character.customData.elevenLabsApiKey || ""},
+    elevenLabsApiKey: {show:d=>d.voiceAudioEnabled=="yes", type:"textLine", label: "Enter your ElevenLabs API key (see <a href='//beta.elevenlabs.io/speech-synthesis'>user settings</a>):", defaultValue:oc.character.customData.elevenLabsApiKey || ""},
   }, {cancelButtonText:null});
   
   oc.character.customData.vrmUrl = result.vrmUrl;
@@ -155,7 +155,7 @@ async function textToSpeechAndActions(text) {
     if(chunk.text) {
       let bufferPromise;
       if(oc.character.customData.voiceAudioEnabled === "yes") {
-        bufferPromise = fetch(`https://api.elevenlabs.io/v1/text-to-speech/${oc.character.customData.elevenLabsVoiceId}?optimize_streaming_latency=0`, {
+        bufferPromise = fetch(`//api.elevenlabs.io/v1/text-to-speech/${oc.character.customData.elevenLabsVoiceId}?optimize_streaming_latency=0`, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -174,7 +174,7 @@ async function textToSpeechAndActions(text) {
       } else {
         let numWords = chunk.text.split(" ").length / 2;
         let filename = roundToNearestOfSet(numWords, [3, 6, 12, 24]) + ".mp3";
-        bufferPromise = fetch(`https://cdn.jsdelivr.net/gh/josephrocca/ChatVRM-js@v0.0.22/OpenCharacters/dummy-audio/${filename}`).then(r => r.arrayBuffer());
+        bufferPromise = fetch(`//cdn.jsdelivr.net/gh/josephrocca/ChatVRM-js@v0.0.22/OpenCharacters/dummy-audio/${filename}`).then(r => r.arrayBuffer());
       }
       speechActionChunks.push({bufferPromise, expression:lastExpression, text:chunk.text, volume});
     }
